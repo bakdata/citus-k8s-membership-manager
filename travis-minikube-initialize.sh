bfcfdf04e0a90218311b9c7c5bbf33fe2258b02e
Fabian Paul <fabian.paul@student.hpi.de>
2018-11-29 09:53:46 +0100

    Log waiting progress

--- travis-minikube-initialize.sh
+++ travis-minikube-initialize.sh
@@ -20,16 +20,17 @@ sudo -E minikube start --vm-driver=none --extra-config=apiserver.InsecureServing
 KUBECTL_UP="false"
 for i in {1..150} # timeout for 5 minutes
 do
-   kubectl get po &> /dev/null
-   if [ $? -ne 1 ]; then
-      KUBECTL_UP="true"
-      break
-  fi
-  sleep 2
+    echo "Waiting for API server"
+    kubectl get po &> /dev/null
+    if [ $? -ne 1 ]; then
+        KUBECTL_UP="true"
+        break
+    fi
+    sleep 2
 done
 if [ "$KUBECTL_UP" != "true" ]; then
-  echo "INIT FAILURE: kubectl could not reach api-server in allotted time"
-  exit 1
+    echo "INIT FAILURE: kubectl could not reach api-server in allotted time"
+    exit 1
 fi
 # kubectl commands are now able to interact with minikube cluster
 
@@ -38,22 +39,23 @@ fi
 KUBE_ADDONS_UP="false"
 for i in {1..150} # timeout for 5 minutes
 do
-     # Here we are making sure that kubectl is returning the addon pods for the namespace kube-system
-     # Without this check, the second if statement won't be in the proper state for execution
-     if [[ $(kubectl get po -n kube-system -l k8s-app=kube-dns | tail -n +2 | grep "kube-dns") ]]; then
-       # Here we are taking the checking the number of running pods for the namespace kube-system
-       # and making sure that the value on each side of the '/' is equal (ex: 3/3 pods running)
-       # this is necessary to ensure that all addons have come up
-       if [[ ! $(kubectl get po -n kube-system | tail -n +2 | awk '{print $2}' | grep -wEv '^([1-9]+)\/\1$') ]]; then
-         echo "INIT SUCCESS: all kubernetes addons pods are up and running"
-         KUBE_ADDONS_UP="true"
-         break
-     fi
+    echo "Waiting for API server"
+    # Here we are making sure that kubectl is returning the addon pods for the namespace kube-system
+    # Without this check, the second if statement won't be in the proper state for execution
+    if [[ $(kubectl get po -n kube-system -l k8s-app=kube-dns | tail -n +2 | grep "kube-dns") ]]; then
+        # Here we are taking the checking the number of running pods for the namespace kube-system
+        # and making sure that the value on each side of the '/' is equal (ex: 3/3 pods running)
+        # this is necessary to ensure that all addons have come up
+        if [[ ! $(kubectl get po -n kube-system | tail -n +2 | awk '{print $2}' | grep -wEv '^([1-9]+)\/\1$') ]]; then
+            echo "INIT SUCCESS: all kubernetes addons pods are up and running"
+            KUBE_ADDONS_UP="true"
+            break
+        fi
    fi
   sleep 2
 done
 if [ "$KUBE_ADDONS_UP" != "true" ]; then
-  echo "INIT FAILURE: kubernetes addons did not come up in allotted time"
-  exit 1
+    echo "INIT FAILURE: kubernetes addons did not come up in allotted time"
+    exit 1
 fi
 # kube-addons is available for cluster services
